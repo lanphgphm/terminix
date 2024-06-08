499b3ad387b9a0bb417d4bffa761b133152c9fa0
lanphgphm <phuong.pham.210120@student.fulbright.edu.vn>
2024-06-09 02:58:39 +0700

    some comments

--- cpp/ptty.cpp
+++ cpp/ptty.cpp
@@ -13,13 +13,16 @@ Ptty::~Ptty(){
 }
 
 void Ptty::executeCommand(QString command){
-    QProcess process;
-    process.start("bash", QStringList() << "-c" << command);
-    process.waitForFinished(-1);
+    // -----------------------------------------
+    // QProcess process;
+    // process.start("bash", QStringList() << "-c" << command);
+    // process.waitForFinished(-1);
 
-    QString result = process.readAllStandardOutput();
-    QString error = process.readAllStandardError();
+    // QString result = process.readAllStandardOutput();
+    // QString error = process.readAllStandardError();
 
-    if (!error.isEmpty()) result += "\Error:\n" + error;
+    // if (!error.isEmpty()) result += "\Error:\n" + error;
+    // -----------------------------------------
+    QString result = "Your command is: " + command; // to debug UI
     emit resultReceivedFromBash(result);
 }
